d08356e798b3ec3f036f388ef62f4bd145deda92
cafeway <bcaranja@gmail.com>
2021-04-18 22:32:29 +0300

    still on netlify lambda

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,25 +1,18 @@
-var axios = require('axios').default;
+const fetch = require("node-fetch");
 
-var options = {
-    
-  }
-exports.handler = async function(event, context) {
-    var response = null
-    axios({
-        method: 'GET',
-        url: 'https://api.proxyapi.co.ke/sandbox/mpesa/v1/auth',
-        headers: {
-          'X-Authorization': 'Basic 40ca323c8f19bd3c34df48320881110de55fc3f48344e0000d1c0144fb66235b',
-          'Content-Type': 'application/json'
-}
-    }).then(function (response) {
-        response = response
-        console.log(response);
-    }).catch(function (error) {
-        console.log(error);
+const API_ENDPOINT = "https://api.proxyapi.co.ke/sandbox/mpesa/v1/auth";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT,{
+       headers: { 
+        'X-Authorization': 'Basic 40ca323c8f19bd3c34df48320881110de55fc3f48344e0000d1c0144fb66235b',
+        'Content-Type': 'application/json'
+        } 
     })
-    return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-    };
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data.joke,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 }
