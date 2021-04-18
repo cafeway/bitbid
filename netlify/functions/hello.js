2588c965f6de0792dbea5fd988d578876207f3b1
cafeway <bcaranja@gmail.com>
2021-04-18 22:23:17 +0300

    still on netlify lambda

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -4,19 +4,22 @@ var options = {
     
   }
 exports.handler = async function(event, context) {
+    var response = null
+    axios({
+        method: 'GET',
+        url: 'https://api.proxyapi.co.ke/sandbox/mpesa/v1/auth',
+        headers: {
+          'X-Authorization': 'Basic 40ca323c8f19bd3c34df48320881110de55fc3f48344e0000d1c0144fb66235b',
+          'Content-Type': 'application/json'
+}
+    }).then(function (response) {
+        response = response
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    })
     return {
         statusCode: 200,
-        body: axios({
-            method: 'GET',
-            url: 'https://api.proxyapi.co.ke/sandbox/mpesa/v1/auth',
-            headers: {
-              'X-Authorization': 'Basic 40ca323c8f19bd3c34df48320881110de55fc3f48344e0000d1c0144fb66235b',
-              'Content-Type': 'application/json'
-    }
-        }).then(function (response) {
-            console.log(response);
-        }).catch(function (error) {
-            console.log(error);
-        })
+        body: JSON.stringify(response)
     };
 }
