70b89087e01706943b353ee412159d550b48786e
cafeway <bcaranja@gmail.com>
2021-04-18 20:28:29 +0300

    added an axios request to my netlify function

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,22 @@
+var axios = require('axios').default;
+
+var options = {
+    
+  }
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: axios({
+            method: 'GET',
+            url: 'https://api.proxyapi.co.ke/sandbox/mpesa/v1/auth',
+            headers: {
+              'X-Authorization': 'Basic 40ca323c8f19bd3c34df48320881110de55fc3f48344e0000d1c0144fb66235b',
+              'Content-Type': 'application/json'
+    }
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        })
     };
 }
