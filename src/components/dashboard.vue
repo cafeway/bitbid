d45b90d1cf92bf4bdb98eb0094465f5d0f756cb0
cafeway <bcaranja@gmail.com>
2021-05-21 22:26:09 -0400

    added a new duration and then added a withdraw modal

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -294,24 +294,28 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Initiate B2C</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="refresh()">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
          <form>
             <div class="form-group">
-              <input type="number" class="form-control" id="number" aria-describedby="emailHelp" placeholder="Enter the Desired amount">
+              <input type="number" class="form-control" id="cashout" v-model="form.cashout" aria-describedby="emailHelp" placeholder="Enter the Desired amount">
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Phone number">
+              <input type="text" v-model="form.number" class="form-control" id="number" placeholder="Phone number">
             </div>
             <div class="form-check">
             </div>
-            <button type="submit" class="btn btn-success">Cashout</button>
+            <button type="button" @click="withdraw()" class="btn btn-success">Cashout</button>
           </form>
       </div>
       <div class="modal-footer">
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="green" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg>
       <h5>Keep it Hortlite!</h5>
       </div>
     </div>
@@ -334,6 +338,11 @@
   <div class="form-group row">
     <div class="col-sm-10">
       <button type="button" @click="submit()" class="btn btn-primary">Invest</button>
+      <button type="button" class="btn-secondary btn btn-sm" @click="refresh()">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-fill" viewBox="0 0 16 16">
+  <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5z"/>
+</svg>
+      refresh</button>
     </div>
   </div>
 </form>
@@ -499,7 +508,9 @@ export default {
         days: 0,
         sharesOnSale: 0,
         bidhours: 0,
-        investment: 0
+        investment: 0,
+        cashout: 0,
+        number: ''
       }
     }
   },
@@ -532,6 +543,26 @@ export default {
     document.head.appendChild(externalScript)
   },
   methods: {
+    withdraw: function () {
+      let db = firebase.firestore()
+      let cashout = this.form.cashout
+      let number = this.form.number
+      let balance = this.wallet - cashout
+      if (cashout <= this.wallet) {
+        db.collection('users').doc(this.user.data.email).update({
+          wallet_balance: balance
+        })
+        this.$vs.notify({title: 'Your Withdrawal was initiated...please wait!', text: this.form.cashout, color: 'blue', position: 'bottom-center'})
+        db.collection('cashout').add({
+          amount: cashout,
+          number: number,
+          email: this.user.data.email,
+          sent: false
+        })
+      } else {
+        this.$vs.notify({title: 'Your withdrawal exceeds the balance!', text: 'please recharge', color: 'red', position: 'bottom-center'})
+      }
+    },
     toInvestment: function () {
       this.$router.push('/investments')
     },
@@ -576,8 +607,8 @@ export default {
       })
       return timer
     },
-    reload: function () {
-      window.navigator.reload()
+    refresh: function () {
+      window.location.reload()
     },
     submit () {
       let db = firebase.firestore()
@@ -589,13 +620,24 @@ export default {
         // eslint-disable-next-line camelcase
         let cust_id = parseFloat(this.total_bids) + 1
         var startdate = firebase.firestore.Timestamp.now().seconds
-        var maturedate = startdate + 86400
-        var rate = this.radio / 100
+        var rate = parseFloat(this.radio / 100)
+        var duration = parseFloat(this.radio)
+        var maturedate = 0
+        console.log(duration)
+        switch (duration) {
+          case 25:
+            maturedate = startdate + 172800
+            break
+          case 35:
+            maturedate = startdate + 259200
+            break
+          default:
+            maturedate = startdate + 86400
+    // code block
+        }
         var amount = parseFloat(this.form.investment)
         var profit = rate * amount
         var total = profit + amount
-        console.log(amount)
-        console.log(profit)
         db.collection('users').doc(this.user.data.email).collection('investments').add({
           id: cust_id,
           amount: total,
