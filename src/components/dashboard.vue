f7f90db9d121972cfa6066a5d5c8dc184bf33084
brian brayo <bcaranja@gmail.com>
2022-03-27 13:24:28 +0300

    Update dashboard.vue

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -45,43 +45,7 @@
             <div class="col-md-12">
                 <div class="dashboard-area">
                     <div class="container-fluid">
-                    <div class="row">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                                        <div class="loader">
-  <div class="loader__image">
-    <div class="loader__coin">
-  <img src="https://www.dropbox.com/s/fzc3fidyxqbqhnj/loader-coin.png?raw=1" alt="">
-  </div>
-    <div class="loader__hand">
-  <img src="https://www.dropbox.com/s/y8uqvjn811z6npu/loader-hand.png?raw=1" alt="">
-    </div>
-  </div>
-</div>
- <h4 class="text-secondary">{{this.wallet}}<small><sub>Ksh</sub></small></h4>
- <p class="text-secondary"><small>Balance</small></p>
-                                            </div>
-                                              <div class="col-md-4">
-                                              <div class="col-md-6">
-                                              <svg xmlns="http://www.w3.org/2000/svg" width="80" height="60" fill="blue" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
-  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-  <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-</svg>
-                                              </div>
-                                              <div class="col-md-6"><h4 class="text-secondary">{{this.refferals * 50}}<sub><small>ksh</small></sub></h4><sub ><small> downline bonus</small></sub></div>
-                                              </div>
-                                            <div class="col-md-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="60" fill="#FFD700" class="bi bi-currency-exchange" viewBox="0 0 16 16">
-  <path d="M0 5a5.002 5.002 0 0 0 4.027 4.905 6.46 6.46 0 0 1 .544-2.073C3.695 7.536 3.132 6.864 3 5.91h-.5v-.426h.466V5.05c0-.046 0-.093.004-.135H2.5v-.427h.511C3.236 3.24 4.213 2.5 5.681 2.5c.316 0 .59.031.819.085v.733a3.46 3.46 0 0 0-.815-.082c-.919 0-1.538.466-1.734 1.252h1.917v.427h-1.98c-.003.046-.003.097-.003.147v.422h1.983v.427H3.93c.118.602.468 1.03 1.005 1.229a6.5 6.5 0 0 1 4.97-3.113A5.002 5.002 0 0 0 0 5zm16 5.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0zm-7.75 1.322c.069.835.746 1.485 1.964 1.562V14h.54v-.62c1.259-.086 1.996-.74 1.996-1.69 0-.865-.563-1.31-1.57-1.54l-.426-.1V8.374c.54.06.884.347.966.745h.948c-.07-.804-.779-1.433-1.914-1.502V7h-.54v.629c-1.076.103-1.808.732-1.808 1.622 0 .787.544 1.288 1.45 1.493l.358.085v1.78c-.554-.08-.92-.376-1.003-.787H8.25zm1.96-1.895c-.532-.12-.82-.364-.82-.732 0-.41.311-.719.824-.809v1.54h-.005zm.622 1.044c.645.145.943.38.943.796 0 .474-.37.8-1.02.86v-1.674l.077.018z"/>
-</svg>
-<h4 class="text-secondary">{{this.cashout}}<small><sub>Ksh</sub></small></h4>
- <p class="text-secondary"><small>Amount Earned</small></p>
-                                            </div>
-                                        </div>
-                            </div>
-                        </div>
-                    </div>
+                   
                     <div class="row">
                         <div class="col-12">
                             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
