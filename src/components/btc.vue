1a5be3d5542849fa2be8cfb0a99b8a9f6b1a350e
cafeway <bcaranja@gmail.com>
2022-04-22 16:08:06 +0300

    ...

--- src/components/btc.vue
+++ src/components/btc.vue
@@ -5,7 +5,7 @@
       <div class="card-header card-header-divider text-center pt-4"> <img src="https://apirone.com/static/promo/bitcoin_logo_vector.svg" class="img-fluid" style="max-height: 42px;" width="205" alt=""><br>
       <img src="https://apirone.com/api/v1/qr?message=1DonateWffyhwAjskoEwXt83pHZxhLTr8H&format=svg" style="max-width: 190px;" alt=""> </div>
       <div class="card-body px-0">
-        <p class="text-center"><small><strong>31r5QreLwu1aFHR7WNdSZVovMXPNyJ4xAf</strong></small></p>
+        <p class="text-center"><strong>31r5QreLwu1aFHR7WNdSZVovMXPNyJ4xAf</strong></p>
         <p class="text-muted text-center">Scan QR code and top-up your<br>
           After payments email the admin<br> <sub><b>dreamcreation74@gmail.com</b></sub> <br>for confirmation<br>
           Payment will be credited after confirmation by the admin.<br>
